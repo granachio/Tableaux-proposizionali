1375338395c276fcd2de07ce66b0fe18b3a76292
Gabriele <chiodi637@gmail.com>
2019-02-10 10:12:50 +0100

    Validate Not, Validate And

--- src/App.js
+++ src/App.js
@@ -1,28 +1,109 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 class App extends Component {
+  state = {
+    formula: '',
+  };
+  validateNot = () => {
+    var reg = new RegExp('[^()&|>-][()]*!')
+    let stringa = this.state.formula;
+    if(reg.test(stringa))
+      alert("not valid not 1");
+
+    reg = new RegExp('![()]*[^()ft]');
+    if(reg.test(stringa))
+      alert("not valid not 2");
+
+    reg = new RegExp('![()]*[tf][()]*[^()A-Z]');
+    if(reg.test(stringa))
+      alert("not valid not 3");
+
+    reg = new RegExp('![()]*[tf][()]*[A-Z][()]*[^()&|>-]+');
+    if(reg.test(stringa))
+      alert("not valid not 4");
+    
+    /* var myString = this.state.formula;
+    var myRegexp = /[^()&|>-][()]*!/g;
+    var match = myRegexp.exec(myString);
+    let i = 0;
+    if(match !== null) {
+      if(match.length >= 1) {
+        while (match[i] !== undefined) {
+          alert(match[i]);
+          i++;
+        }
+      }
+    } */
+  };
+
+  validateAnd = () => {
+    // Left Side.
+    var reg = new RegExp('[^()A-Z][()]*&');
+    let stringa = this.state.formula;
+    if(reg.test(stringa))
+      alert("not valid and left side 1");
+    
+    reg = new RegExp('[^()ft]+[()]*[A-Z][()]*&');
+    stringa = this.state.formula;
+    if(reg.test(stringa))
+      alert("not valid and left side 2");
+
+    reg = new RegExp('[^()!&|>-][()]*[ft][()]*[A-Z][()]*&');
+    stringa = this.state.formula;
+    if(reg.test(stringa))
+      alert("not valid and left side 3");
+
+    // Right Side.
+    reg = new RegExp('&[()!]*[^()!ft]');
+    stringa = this.state.formula;
+    if(reg.test(stringa))
+      alert("not valid and right side 1");
+
+    reg = new RegExp('&[()!]*[ft][()]*[^()A-Z]');
+    stringa = this.state.formula;
+    if(reg.test(stringa))
+      alert("not valid and right side 2");
+    
+    reg = new RegExp('&[()!]*[ft][()]*[A-Z][()]*[^()&|>-]');
+    stringa = this.state.formula;
+    if(reg.test(stringa))
+      alert("not valid and right side 3");
+
+  };
+  validateAll = () => {
+    this.validateNot();
+    this.validateAnd();
+  };
+  cambia = (name) => event => {
+    this.setState({[name]: event.target.value});
+  };
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <center>
+      <table>
+        <tr>
+          <td>
+            <TextField
+              id="outlined-name"
+              label="Check not"
+              margin="normal"
+              variant="outlined"
+              onChange={this.cambia('formula')}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <Button variant="outlined" onClick={this.validateAll}>Click</Button>
+          </td>
+        </tr>
+      </table>
+      </center>
     );
   }
 }
-
 export default App;
+ 
